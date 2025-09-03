76d1602d67c0a10fbe52cc6595c2bb4b9a8cb19f
MarwaKhalifa <marwakhalifa39@gmail.com>
2025-09-03 03:14:24 +0300

    error update

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,5 @@
 import logo from './assets/logo.png'
-import settingImg from './assets/Settings container.png'
+import settingImg from './assets/Settings-Container.png'
 export default function Header(){
     return(
         <header className="container flex items-center justify-between p-3 bg-[#FBFDFE]  w-full max-[w-1170px] h-[74px] border-1 border-[#D6E2F5] rounded-3xl shadow-[#D9E5F4] shadow-2xl mt-10 ">
